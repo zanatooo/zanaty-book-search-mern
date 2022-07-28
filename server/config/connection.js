2486a94b2cc0b951c0b8e1bc9bd4624c7171633c
zanatooo <m8elzanaty@gmail.com>
2022-07-27 20:38:38 -0700

    fixing connection errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+require("dotenv").config()
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/bookSearch", 
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
