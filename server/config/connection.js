5b9091c0b574e4e57bdcec93c550b5d135e7e245
zanatooo <m8elzanaty@gmail.com>
2022-07-30 08:28:21 -1000

    fixing the dotenv connections

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 console.log(process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/bookSearch", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:3000/bookSearch", 
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require("dotenv").config()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3001/bookSearch";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3000/bookSearch";
 const server = new ApolloServer({
   typeDefs,
   resolvers,
