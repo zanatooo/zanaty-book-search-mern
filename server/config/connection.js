6aab7c489d72757383cd7a0a738a4124b1909f4b
zanatooo <m8elzanaty@gmail.com>
2022-07-31 16:19:19 -1000

    editing connection to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-require("dotenv").config()
-console.log(process.env.MONGODB_URI)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:3000/bookSearch", 
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
